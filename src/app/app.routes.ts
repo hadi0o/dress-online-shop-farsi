d56efc0668c5d18562c8508b0a6216eb4efe822f
hadi0o <hadi.deh19@gmail.com>
2024-08-18 23:05:45 +0330

    implemented home page and angular.json codes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./modules/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path: '', component: HomeComponent},
+   // {path: 'about', component: AboutComponent},
+   // {path: 'comments', component: CommentsComponent},
+   // {path: 'contact', component: ContactComponent},
+   // {path: 'products', component: ProductsComponent},
+
+    {path: '**', redirectTo: ''}
+];
